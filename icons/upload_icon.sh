1888965a83ab1fdc5ba4b57eab54bb34d87be322
Jeff Stein <jstein@mitre.org>
2023-06-16 20:36:38 -0600

    feat: Fixed the stupid broken script of crap

--- icons/upload_icon.sh
+++ icons/upload_icon.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 #
 # Call this script with:
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/jeeftor/HomeAssistant/master/icons/upload_icon.sh)"
@@ -26,30 +28,55 @@ list_icons() {
     DIRECTORY="icons/$1"
 
     # Make the API request to list the directory contents
-    curl -s "https://api.github.com/repos/$OWNER/$REPO/contents/$DIRECTORY?ref=$BRANCH" | jq -r '.[].download_url'
+    curl -L -s "https://api.github.com/repos/$OWNER/$REPO/contents/$DIRECTORY?ref=$BRANCH" | jq -r '.[].download_url'
 }
 
+verify_gif() {
+    local FILE_NAME="$1"
+
+    gif_header=$(head -c 6 "$FILE_NAME" | od -An -t x1 | tr -d ' ')
+    if echo "$gif_header" | grep -q "^474946383961"; then
+        # echo "File $FILE_NAME appears to be a valid a GIF file."
+        return 0
+    else
+        echo "Error: File $FILE_NAME is NOT a valid GIF file."
+        return 1
+    fi
+}
 upload_icon() {
-    IP_ADDRESS="$1"
-    ICON_NAME="$2"
-    FILE_NAME="$3"
+    local IP_ADDRESS="$1"
+    local ICON_NAME="$2"
+    local FILE_NAME="$3"
+    echo FILE_NAME=$FILE_NAME
+
     URL="http://$IP_ADDRESS/edit"
 
-    TEMP_FILE=$(mktemp)
+    TEMP_FILE=".$FILE_NAME"
 
     BASE_URL="https://raw.githubusercontent.com/jeeftor/HomeAssistant/master/icons/"
     GIF_FILE="$BASE_URL/$ICON_NAME"
 
-    curl -s -X GET "$GIF_FILE" -o "$TEMP_FILE"
-    curl -X POST -F "file=@$TEMP_FILE;filename=/ICONS/$FILE_NAME" "$URL"
-    echo "Uploaded icon: $ICON_NAME"
+    curl -L -s -X GET "$GIF_FILE" -o "$TEMP_FILE"
 
-    rm "$TEMP_FILE"
+    if verify_gif "$TEMP_FILE"; then
+        curl -X POST -F "file=@$TEMP_FILE;filename=/ICONS/$FILE_NAME" "$URL"
+        echo "Uploaded icon: $FILE_NAME"
+    else
+        echo "Error: File $TEMP_FILE is not a valid GIF file."
+    fi
+
+    rm -f "$TEMP_FILE"
+
 }
 
 # Prompt for IP address
 read -p "Enter the IP address: " IP_ADDRESS
 
+# Validate IP address format
+if ! [[ $IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error: Invalid IP address format."
+    exit 1
+fi
 # List icon directories
 echo "Available icon directories:"
 directories=($(list_icon_directories))
@@ -67,9 +94,10 @@ done
 # Example usage
 ICONS=($(list_icons "$DIRECTORY_NAME"))
 
+echo "Downloading $ICONS"
+
 for ICON_URL in "${ICONS[@]}"; do
     ICON_NAME=$(basename "$ICON_URL")
-    FILE_NAME="$ICON_NAME"
 
     upload_icon "$IP_ADDRESS" "$DIRECTORY_NAME/$ICON_NAME" "$FILE_NAME"
 done
