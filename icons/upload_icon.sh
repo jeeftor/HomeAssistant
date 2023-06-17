f9e416e60ba168b986179369b78319d42b45fcb0
Jeff Stein <jstein@mitre.org>
2023-06-16 20:46:38 -0600

    feat: Adding colors to upload script

--- icons/upload_icon.sh
+++ icons/upload_icon.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 #
 # Call this script with:
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/jeeftor/HomeAssistant/master/icons/upload_icon.sh)"
@@ -20,34 +26,38 @@ list_icon_directories() {
     # Return the list of directories
     echo "${directories[@]}"
 }
-
 list_icons() {
-    OWNER="jeeftor"
-    REPO="HomeAssistant"
-    BRANCH="master"
-    DIRECTORY="icons/$1"
+    local OWNER="jeeftor"
+    local REPO="HomeAssistant"
+    local BRANCH="master"
+    local DIRECTORY="icons/$1"
 
     # Make the API request to list the directory contents
-    curl -L -s "https://api.github.com/repos/$OWNER/$REPO/contents/$DIRECTORY?ref=$BRANCH" | jq -r '.[].download_url'
+    local response=$(curl -L -s "https://api.github.com/repos/$OWNER/$REPO/contents/$DIRECTORY?ref=$BRANCH")
+
+    # Extract URLs of files with the .gif extension
+    local icons=($(echo "$response" | jq -r '.[] | select(.type == "file" and (.name | test("\\.gif$"))) | .download_url'))
+
+    # Return the list of icons
+    echo "${icons[@]}"
 }
 
 verify_gif() {
     local FILE_NAME="$1"
 
-    gif_header=$(head -c 6 "$FILE_NAME" | od -An -t x1 | tr -d ' ')
-    if echo "$gif_header" | grep -q "^474946383961"; then
-        # echo "File $FILE_NAME appears to be a valid a GIF file."
+    if file -b --mime-type "$FILE_NAME" | grep -q '^image/gif$'; then
+        # File is a valid GIF
         return 0
     else
-        echo "Error: File $FILE_NAME is NOT a valid GIF file."
+        echo -e "${RED}Error: File $FILE_NAME is NOT a valid GIF file.${NC}"
         return 1
     fi
 }
+
 upload_icon() {
     local IP_ADDRESS="$1"
     local ICON_NAME="$2"
     local FILE_NAME="$3"
-    echo FILE_NAME=$FILE_NAME
 
     URL="http://$IP_ADDRESS/edit"
 
@@ -60,13 +70,13 @@ upload_icon() {
 
     if verify_gif "$TEMP_FILE"; then
         curl -X POST -F "file=@$TEMP_FILE;filename=/ICONS/$FILE_NAME" "$URL"
-        echo "Uploaded icon: $FILE_NAME"
+        echo -e "${GREEN}Uploaded icon:${NC} $FILE_NAME${NC}"
     else
-        echo "Error: File $TEMP_FILE is not a valid GIF file."
+        echo -e "${RED}Error: File $FILE_NAME does not appear to be a valid GIF file.${NC}"
+        echo -e ${RED}Try yourself with:${NC} curl -L -s -X GET "$GIF_FILE" -o $TEMP_FILE
     fi
 
     rm -f "$TEMP_FILE"
-
 }
 
 # Prompt for IP address
@@ -74,11 +84,12 @@ read -p "Enter the IP address: " IP_ADDRESS
 
 # Validate IP address format
 if ! [[ $IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Error: Invalid IP address format."
+    echo -e "${RED}Error: Invalid IP address format.${NC}"
     exit 1
 fi
+
 # List icon directories
-echo "Available icon directories:"
+echo -e "${GREEN}Available icon directories:${NC}"
 directories=($(list_icon_directories))
 
 # Prompt for directory selection
@@ -87,17 +98,17 @@ select DIRECTORY_NAME in "${directories[@]}"; do
     if [[ -n $DIRECTORY_NAME ]]; then
         break
     else
-        echo "Invalid selection. Please try again."
+        echo -e "${YELLOW}Invalid selection. Please try again.${NC}"
     fi
 done
 
 # Example usage
 ICONS=($(list_icons "$DIRECTORY_NAME"))
 
-echo "Downloading $ICONS"
+echo -e "${YELLOW}Downloading icons:${NC}"
 
 for ICON_URL in "${ICONS[@]}"; do
     ICON_NAME=$(basename "$ICON_URL")
 
-    upload_icon "$IP_ADDRESS" "$DIRECTORY_NAME/$ICON_NAME" "$FILE_NAME"
+    upload_icon "$IP_ADDRESS" "$DIRECTORY_NAME/$ICON_NAME" "$ICON_NAME"
 done
